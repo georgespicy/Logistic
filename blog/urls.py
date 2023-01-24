b4b44a545f8d3b21f2f1e106f52c3d8a0e1030bb
Israel Akinoso <akinosoissy@gmail.com>
2023-01-24 15:01:05 +0100

    Blog and blog details partially completed

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import blog, blog_detail
 
 urlpatterns = [
-    path("blog/", blog, name="blog"),
-    path("blog_detail/", blog_detail, name="blog_detail"),
+    path("", blog, name="blog"),
+    path("<slug:slug>/", blog_detail, name="blog_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Post
 
 def blog(request):
     categories = Category.objects.all()[:6]
+    posts = Post.objects.all()
     context = {
-        'categories': categories
+        'categories': categories,
+        'posts': posts
     }
     return render(request, 'blog/blog.html', context)
 
-def blog_detail(request):
-    return render(request, 'blog/blog_detail.html')
+def blog_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    categories = Category.objects.all()[:6]
+    context = {
+        'categories': categories,
+        'post': post,
+        
+    }
+    return render(request, 'blog/blog_detail.html', context)
